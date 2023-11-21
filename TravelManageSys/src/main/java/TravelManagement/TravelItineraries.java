3100642f8b0168398452d4a31532a4a966ccc5a3
Abdelrahman <>
2023-11-21 20:32:09 +0200

    File Structure Done

---
+++ TravelManageSys/src/main/java/TravelManagement/TravelItineraries.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TravelManagement;
+
+/**
+ *
+ * @author bmood
+ */
+public class TravelItineraries {
+    //consists of a template for the users to view their travel path or journey
+    //Allow users to make changes to their bookings, such as rescheduling or canceling.
+    
+}
