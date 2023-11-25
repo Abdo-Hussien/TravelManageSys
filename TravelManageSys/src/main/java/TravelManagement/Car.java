2079554828c4756ea43b8f5c098febc3ce4faed7
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2023-11-25 21:27:12 +0200

    added getter in car file

--- TravelManageSys/src/main/java/TravelManagement/Car.java
+++ TravelManageSys/src/main/java/TravelManagement/Car.java
@@ -6,4 +6,25 @@ public class Car {
     String type;
     int noSeats;
     double price=type.equals("suv") ? 0.3 : 0.2;
+
+    public String getMade() {
+        return this.made;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getNoSeats() {
+        return this.noSeats;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
 }
