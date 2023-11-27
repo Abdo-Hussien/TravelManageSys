01598be3d9a0f9a5f1ae7234ebe19cf5fefb5f2f
Mohamed <b.moody2004@gmail.com>
2023-11-27 02:10:13 +0200

    Removed DummyData in CustomerBooking Class

--- TravelManageSys/src/main/java/TravelManagement/CustomerBooking.java
+++ TravelManageSys/src/main/java/TravelManagement/CustomerBooking.java
@@ -5,7 +5,13 @@
 package TravelManagement;
 
 import java.util.Date;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,7 +23,7 @@ import java.util.List;
  * @author bmood
  */
 public class CustomerBooking {
-    // private List<Trip> tripsList = Trip.getAllTrips();
+    private ArrayList<Trip> tripsList = getAllTrips();
     private int TripCounter;
     private int DiffTripCounter;
     // Filters
@@ -27,28 +33,76 @@ public class CustomerBooking {
     private String start_date;
     private String end_date;
 
-    // private List<Trip> filteredTrips = new ArrayList<>();
+    private ArrayList<Trip> filteredTrips = new ArrayList<>();
 
-    // Return Type should be: List<Trip>
-    public void getFilteredTrips(String search_filter) {
+    public List<Trip> getFilteredTrips(String search_filter) {
         String[] Filters = search_filter.split("/");
+        // Split Filters.
         setFilters(Filters);
+
         // Iterate through the trips and filter based on the search_text
-
-        // Add this argument at the first when merging: trip,
-        // for (Trip trip : tripsList) {
-        if (tripSearch(search_text, start_date, end_date, price_start, price_end)) {
-            // filteredTrips.add(trip);
-            System.out.println("Trip Displayed Successfully");
-        } else
-            System.out.println("Trip wasn't displayed");
-        // }
-        // return filteredTrips;
-        
-        // FileWriter file = new FileWriter("TravelManageSys/src/data/customer_{id}.txt", true);
-
+        for (Trip trip : tripsList) {
+            if (tripSearch(trip, search_text, start_date, end_date, price_start, price_end)) {
+                // filteredTrips.add(trip);
+                System.out.println("Trip Displayed Successfully");
+            } else
+                System.out.println("Trip wasn't displayed");
+        }
+        return filteredTrips;
         // Checkout no cancellation - Save in a file.
         // Booking Save in runtime variables (Array List) - Not save in a file.
+    }
+
+    private ArrayList<Trip> getAllTrips() {
+
+        try {
+            Path tripsFile = Path.of("TravelManageSys/src/data/trips.txt");
+            Trip trip = parseTrip(Files.readString(tripsFile));
+            if (trip != null)
+                tripsList.add(trip);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return tripsList;
+    }
+
+    private Trip parseTrip(String line) {
+        String[] parts = line.split("\n");
+
+        // Invalid format, return null or handle appropriately
+        // if (parts.length != {Number of Lines for each Trip}) {
+        // return null;
+        // }
+
+        try {
+            String id = parts[0];
+            String tripName = parts[1];
+            String type = parts[2];
+            double price = Double.parseDouble(parts[3]);
+            String startDate = parts[4];
+            String endDate = parts[5];
+            String description = parts[6];
+            int capacity = Integer.parseInt(parts[7]);
+            // Create a Trip object based on the type
+            switch (type) {
+                case "GeneralTour":
+                    return new GeneralTours(id, tripName, type, price, startDate, endDate, description, null,
+                            capacity, null, null, null, null);
+                case "FamilyTour":
+                    return new FamilyTours(id, tripName, type, price, startDate, endDate, description, null,
+                            capacity, null, null, null, null);
+                case "CoupleTour":
+                    return new CoupleTours(id, tripName, type, price, startDate, endDate, description, null,
+                            capacity, null, null, null, null);
+                default:
+                    // Handle unknown type or return null
+                    return null;
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace(); // Handle the exception appropriately, e.g., log or throw
+            return null;
+        }
     }
 
     private void setFilters(String[] Filters) {
@@ -79,26 +133,30 @@ public class CustomerBooking {
     }
 
     // Add this parameter for each overload at the first when merging: Trip trip
-    private boolean tripSearch(String search_filter) {
+    private boolean tripSearch(Trip trip, String search_filter) {
         if (search_filter == null)
             return true;
-        String desc = new String(
-                "an Egyptian town in the Suez Governorate, lying on the western shore of the Red Sea's Gulf of Suez. Being a year-round sunshine destination with gentle waves of the clear Red Sea makes Al-Ain al-Sokhna an ideal destination to break from the city's congestion.");
-        return "Al-Ain Al-Sokhna".toLowerCase().contains(search_filter.toLowerCase()) // trip.getDestination()
-                || "Ain Sokhna".toLowerCase().contains(search_filter.toLowerCase()) // trip.getTripName()
-                || desc.toLowerCase().contains(search_filter.toLowerCase()) // trip.getDescription()
-                || "General Tour".toLowerCase().contains(search_filter.toLowerCase()); // trip.getType()
+        // Destination: Al-Ain
+        // Al-Sokhna.toLowerCase().contains(search_filter.toLowerCase())
+        // String desc = new String(
+        // "an Egyptian town in the Suez Governorate, lying on the western shore of the
+        // Red Sea's Gulf of Suez. Being a year-round sunshine destination with gentle
+        // waves of the clear Red Sea makes Al-Ain al-Sokhna an ideal destination to
+        // break from the city's congestion.");
+        return trip.getTitle().toLowerCase().contains(search_filter.toLowerCase()) // "Ain Sokhna"
+                || trip.getDescription().toLowerCase().contains(search_filter.toLowerCase()) // desc
+                || trip.getTripType().toLowerCase().contains(search_filter.toLowerCase()); // "General Tour"
     }
 
-    private boolean tripSearch(String search_start_date, String search_end_date) {
+    private boolean tripSearch(Trip trip, String search_start_date, String search_end_date) {
         if (search_start_date == null || search_end_date == null)
             return true;
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         try {
             Date start_date = dateFormat.parse(search_start_date);
             Date end_date = dateFormat.parse(search_end_date);
-            Date tripStartDate = dateFormat.parse("27-11-2023"); // trip.getStartDate()
-            Date tripEndDate = dateFormat.parse("29-11-2023"); // trip.getEndDate()
+            Date tripStartDate = dateFormat.parse(trip.getStartDate()); // "27-11-2023"
+            Date tripEndDate = dateFormat.parse(trip.getEndDate()); // "29-11-2023"
             // To reverse this conversion, use this method
             // String DateToString = dateFormat.format(date);
             return (tripStartDate.after(start_date) || tripStartDate.equals(start_date))
@@ -109,15 +167,15 @@ public class CustomerBooking {
 
     }
 
-    private boolean tripSearch(double price_start, double price_end) {
-        double tripPrice = 2849.99; // trip.getPrice()
+    private boolean tripSearch(Trip trip, double price_start, double price_end) {
+        double tripPrice = trip.getInitPrice(); // trip.getPrice() 2849.99
         return tripPrice >= price_start && (price_end > 0 && price_end > price_start) ? tripPrice <= price_end : true;
     }
 
-    // Add this argument at the first when merging: trip,
-    private boolean tripSearch(String search_filter, String start_date, String end_date, double price_start,
+    private boolean tripSearch(Trip trip, String search_filter, String start_date, String end_date, double price_start,
             double price_end) {
-        return tripSearch(price_start, price_end) && tripSearch(start_date, end_date) && tripSearch(search_filter);
+        return tripSearch(trip, price_start, price_end) && tripSearch(trip, start_date, end_date)
+                && tripSearch(trip, search_filter);
     }
 
 }
