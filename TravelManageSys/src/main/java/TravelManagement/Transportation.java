9fef1e093a8ef236f285a454dfd7375e405bd87e
1OneManArmy <bodyatef_122@hotmail.com>
2023-11-28 16:10:48 +0200

    generate transportation classes

--- TravelManageSys/src/main/java/TravelManagement/Transportation.java
+++ TravelManageSys/src/main/java/TravelManagement/Transportation.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,5 +10,37 @@ package TravelManagement;
  * @author bmood
  */
 public class Transportation {
+    private String pickUp;
+    private String destination;
+    private int maxCapacity;
+
+    // Super Constructor
+    public Transportation(String pickUp, String destination, int maxCapacity) {
+        this.pickUp = pickUp;
+        this.destination = destination;
+        this.maxCapacity = maxCapacity;
+    }
+
+    // Getter methods
+    public String getPickUp() {
+        return pickUp;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
     
 }
+
+
+
+
+
+
+
+
+
