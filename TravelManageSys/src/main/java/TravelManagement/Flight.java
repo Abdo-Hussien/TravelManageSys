cccdfd9eb6e3b90b1325b74ad623b270b4944227
1OneManArmy <bodyatef_122@hotmail.com>
2023-11-28 17:06:58 +0200

    data of transportation is done

--- TravelManageSys/src/main/java/TravelManagement/Flight.java
+++ TravelManageSys/src/main/java/TravelManagement/Flight.java
@@ -2,20 +2,15 @@ package TravelManagement;
 
 class Flight extends Transportation {
     private String airline;
-    private String flight_grade;
 
     // Constructor
-    public Flight(String pickUp, String destination, int maxCapacity, String airline, String flight_grade) {
-        super(airline, airline, maxCapacity);
+    public Flight(String pickUp, int maxCapacity, String airline) {
+        super(pickUp, maxCapacity);
         this.airline = airline;
-        this.flight_grade = flight_grade;
     }
 
     public String getAirline() {
         return airline;
     }
-
-    public String getflight_grade() {
-        return flight_grade;
-    }
+    
 }
