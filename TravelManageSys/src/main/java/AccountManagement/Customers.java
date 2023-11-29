754d08417f2b717113d1c51ea3a8ae3789d99070
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2023-11-30 00:37:15 +0200

    deleted id generator from customer class

--- TravelManageSys/src/main/java/AccountManagement/Customers.java
+++ TravelManageSys/src/main/java/AccountManagement/Customers.java
@@ -24,32 +24,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 //contains method for genrating a random ID
-class RandIDGenerator {
-    private StringBuilder str;
-    private int itemCount;
-    private String alphaNumeric;
-
-    public RandIDGenerator() {
-        str = new StringBuilder();
-        itemCount = 0;
-        alphaNumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    }
-
-    public void generateRandID() {
-        Random rand = new Random();
-        for (int i = 0; i < itemCount; i++) {
-            str.append(alphaNumeric.charAt(rand.nextInt(alphaNumeric.length())));
-        }
-    }
-
-    public void setItemCount(int itemCount) {
-        this.itemCount = itemCount;
-    }
-
-    public String getRandID() {
-        return str.toString();
-    }
-}
 
 public class Customers extends Person {
 
