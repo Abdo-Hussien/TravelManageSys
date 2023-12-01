43a0ca4ef9a4d256086615d6886027f52a345766
Mohamed <b.moody2004@gmail.com>
2023-12-01 16:30:00 +0200

    remove main content

--- TravelManageSys/src/main/java/com/asu/main/TravelManageSys.java
+++ TravelManageSys/src/main/java/com/asu/main/TravelManageSys.java
@@ -16,12 +16,6 @@ public class TravelManageSys {
 
     public static void main(String[] args)  {
         System.out.println("Main Class is called 'TravelManageSys' is in the 'main' package");
-        ArrayList<Customers> customers = new ArrayList<>();
-        customers = fileManipulation.getAllCustomers();
-        for (Customers customer : customers) {
-            for (String trip : customer.tripsHistory) {
-                System.out.println(trip);
-            }
-        }
+
     }
 }
